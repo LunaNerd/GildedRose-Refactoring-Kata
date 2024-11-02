2ddd0f4e60e1fca9cd43a5faddd1618345bf190e
luversmi <luna.versmissen@ugent.be>
2024-11-02 17:20:21 +0100

    Refactor done

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,53 +1,59 @@
 package com.gildedrose
 
+import kotlin.math.max
+import kotlin.math.min
+
 class GildedRose(var items: List<Item>) {
 
+    private val SULFURAS = "Sulfuras, Hand of Ragnaros"
+    private val BRIE = "Aged Brie"
+    private val PASS = "Backstage passes to a TAFKAL80ETC concert"
+
+    private val MIN_QUALITY = 0
+    private val MAX_QUALITY = 50
+
+    private val PASS_QUALITY_BUMP_1 = 10
+    private val PASS_QUALITY_BUMP_2 = 5
+
+    fun addOrDetractFromItem(item : Item, change: Int){
+        item.quality += change
+        item.quality = min(item.quality, MAX_QUALITY)     //returns 50 if quality > 50
+        item.quality = max(item.quality, MIN_QUALITY)     //returns 0 if quality < 0
+    }
+
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
+
+        for (item in items) {
+
+            if (item.name != SULFURAS) {
+
+                var qualityDecline = -1
+                var qualityIncrease = 1
+
+                item.sellIn -= 1
+
+                if (item.sellIn < 0) {
+                    qualityDecline *= 2
+                    qualityIncrease *= 2
                 }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
+                when (item.name) {
+                    BRIE -> {
+                        addOrDetractFromItem(item, qualityIncrease)
+                    }
+                    PASS -> {
+                        if (item.sellIn < 0) {
+                            item.quality = 0
+                        } else if (item.sellIn < PASS_QUALITY_BUMP_2) {
+                            addOrDetractFromItem(item, 3)
+                        } else if (item.sellIn < PASS_QUALITY_BUMP_1) {
+                            addOrDetractFromItem(item, 2)
+                        } else {
+                            addOrDetractFromItem(item, 1)
                         }
                     }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    else -> {
+                        addOrDetractFromItem(item, qualityDecline)
                     }
                 }
             }
